cc760a1cad5fcb22eaba81882881c56ddc130283
nahuel61920 <estebancarrizo619@gmail.com>
2026-01-06 12:45:06 -0300

    refactor: :recycle: parce

--- app/Models/News.php
+++ app/Models/News.php
@@ -23,6 +23,10 @@ class News extends Model
         'id_user',
     ];
 
+    protected $casts = [
+        'date' => 'date',
+    ];
+
     protected $dates = ['deleted_at'];
 
     public function plan(): BelongsTo
